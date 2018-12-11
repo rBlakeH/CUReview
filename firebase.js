3b3a305885830cf6f3adf65f8ce64e9533c92f8b
Blake <rblakehampton@gmail.com>
2018-12-10 21:41:44 -0700

    Firebase Login and Table re-work.

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var app_fireBase = {};
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDbYp16ohN1iHduOzUzuDH5ebYmifARqo0",
+    authDomain: "cureview-310d2.firebaseapp.com",
+    databaseURL: "https://cureview-310d2.firebaseio.com",
+    projectId: "cureview-310d2",
+    storageBucket: "cureview-310d2.appspot.com",
+    messagingSenderId: "837144540202"
+  };
+  firebase.initializeApp(config);
+})()
